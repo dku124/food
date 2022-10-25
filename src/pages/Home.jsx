f6b5d989e9c6b6fd4a78630e9953f153651b240d
dku124 <duongkhanhuyen124@gmail.com>
2022-10-25 10:50:35 +0700

    feature section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,16 @@ import '../styles/home.css';
 import { Link } from 'react-router-dom';
 import Category from '../components/UI/Category/Category';
 
+import feature01 from '../assets/images/service-01.png';
+import feature02 from '../assets/images/service-02.png';
+import feature03 from '../assets/images/service-03.png';
+
+const featureData = [
+  { title: 'Quick Delivery', desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.', imgUrl: feature01 },
+  { title: 'Super Dine In', desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.', imgUrl: feature02 },
+  { title: 'Easy Pick Up', desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.', imgUrl: feature03 },
+];
+
 const Home = () => {
   return (
     <Helmet title={'home'}>
@@ -58,6 +68,31 @@ const Home = () => {
 
       <section>
         <Category />
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h5 className="featute__subtitle">What we serve</h5>
+              <h3 className="feature__title">
+                Just sit back at home <br />
+                we will <span>take care</span>
+              </h3>
+              <p className="feature__text mt-4 mb-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum nihil</p>
+              <p className="feature__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum nihil</p>
+            </Col>
+            {featureData.map((item, index) => (
+              <Col lg="4" md="14" className="mt-5">
+                <div className="feature__item text-center px-5">
+                  <img src={item.imgUrl} alt={item.title} className="w-25 mb-3" />
+                  <h5 className="fw-bold">{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </section>
     </Helmet>
   );
